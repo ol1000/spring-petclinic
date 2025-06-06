42504b58b3b87488103f1c7fa555f7880dbf3ef7
Oliver Hunt <ol1000@me.com>
2025-06-06 10:09:22 +0100

    remove error

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -29,9 +29,9 @@ import org.springframework.context.annotation.ImportRuntimeHints;
 @SpringBootApplication
 @ImportRuntimeHints(PetClinicRuntimeHints.class)
 public class PetClinicApplication {
-/* 
+
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
-}*/
+}
