c7a46e5e447d4d1f4bb663d19b6472d1ca35dce5
ol1000 <ol1000@me.com>
2025-06-09 10:15:54 +0100

    Update PetClinicApplication and ensure all simulated error changes are committed

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -14,23 +14,3 @@
  * limitations under the License.
  */
 
-package org.springframework.samples.petclinic;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportRuntimeHints;
-
-/**
- * PetClinic Spring Boot Application.
- *
- * @author Dave Syer
- */
-@SpringBootApplication
-@ImportRuntimeHints(PetClinicRuntimeHints.class)
-public class PetClinicApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PetClinicApplication.class, args);
-	}
-
-}
