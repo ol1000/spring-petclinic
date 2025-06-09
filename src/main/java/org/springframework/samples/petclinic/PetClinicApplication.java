8b582ce74ecd4c97e42b099b1e4aaee9d2259628
ol1000 <ol1000@me.com>
2025-06-09 10:35:31 +0100

    Apply Spring Java formatting fixes to main branch

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -14,25 +14,24 @@
  * limitations under the License.
  */
 
- package org.springframework.samples.petclinic;
+package org.springframework.samples.petclinic;
 
- import org.springframework.boot.SpringApplication;
- import org.springframework.boot.autoconfigure.SpringBootApplication;
- import org.springframework.context.annotation.ImportRuntimeHints;
- 
- /**
-  * PetClinic Spring Boot Application.
-  *
-  * @author Dave Syer
-  *
-  */
- @SpringBootApplication
- @ImportRuntimeHints(PetClinicRuntimeHints.class)
- public class PetClinicApplication {
- 
-     public static void main(String[] args) {
-         SpringApplication.run(PetClinicApplication.class, args);
-     }
- 
- }
- 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
+
+/**
+ * PetClinic Spring Boot Application.
+ *
+ * @author Dave Syer
+ *
+ */
+@SpringBootApplication
+@ImportRuntimeHints(PetClinicRuntimeHints.class)
+public class PetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicApplication.class, args);
+	}
+
+}
