081ecda854855239f1759c0950d0979437bf718d
Oliver Hunt <ol1000@me.com>
2025-06-12 10:26:36 +0100

    new tretsts

--- src/test/java/org/springframework/samples/petclinic/system/CrashControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/system/CrashControllerTests.java
@@ -33,8 +33,8 @@ class CrashControllerTests {
 	final CrashController testee = new CrashController();
 
 	@Test
-	void testTriggerException() {
-		assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> testee.triggerException())
+	void testtriggerGenericException() {
+		assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> testee.triggerGenericException())
 			.withMessageContaining("Expected: controller used to showcase what happens when an exception is thrown");
 	}
 
